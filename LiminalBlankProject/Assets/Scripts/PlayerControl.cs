326a86ce2ca2ea864ed664252facc7f76cdaf601
Gareth <gmorgan27@student.holmesglen.edu.au>
2021-05-08 13:06:29 +1000

    Fix player movement

--- LiminalBlankProject/Assets/Scripts/PlayerControl.cs
+++ LiminalBlankProject/Assets/Scripts/PlayerControl.cs
@@ -1,24 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Liminal.SDK.Core;
 using UnityEngine;
 using Liminal.SDK.VR;
 using Liminal.SDK.VR.Input;
 
 public class PlayerControl : MonoBehaviour
 {
-    private Rigidbody avatarRigidbody;
     [SerializeField] private float movementSpeed;
+    [SerializeField] private Transform avatarEye;
+    private Rigidbody avatarRigidbody;
     private IVRInputDevice primaryInput;
     
     private void Awake()
     {
-        primaryInput = VRDevice.Device.PrimaryInputDevice;
-        avatarRigidbody = GetComponent<Rigidbody>();
+        ExperienceApp.Initializing += () =>
+        {
+            primaryInput = VRDevice.Device.PrimaryInputDevice;
+            avatarRigidbody = GetComponent<Rigidbody>();
+        };
     }
 
     private void Update()
     {
-        var movementVector = primaryInput.GetAxis2D(VRAxis.One).normalized * movementSpeed;
-        avatarRigidbody.velocity = movementVector;
+        #if !DEBUG_VR
+        var movementVector = primaryInput.GetAxis2D(VRAxis.OneRaw).normalized * movementSpeed;
+        #else
+        var movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * movementSpeed;
+        #endif
+
+        var forward = Quaternion.Euler(0f, avatarEye.rotation.eulerAngles.y, 0f);
+        avatarRigidbody.velocity = forward * new Vector3(movementVector.x, 0f, movementVector.y);
     }
 }
