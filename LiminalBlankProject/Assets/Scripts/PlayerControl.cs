f1c454a989aef1b65ba6fbf9f483f61fcb57c240
Gareth <gmorgan27@student.holmesglen.edu.au>
2021-05-11 12:33:40 +1000

    Try hand collisions

--- LiminalBlankProject/Assets/Scripts/PlayerControl.cs
+++ LiminalBlankProject/Assets/Scripts/PlayerControl.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿#define DEBUG_VR_XDDDA
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using Liminal.SDK.Core;
 using UnityEngine;
@@ -11,14 +14,17 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private Transform avatarEye;
     private Rigidbody avatarRigidbody;
     private IVRInputDevice primaryInput;
+    private Collider collider;
     
     private void Awake()
     {
-        // I'm not 100% sure when the primary input device becomes available. This is to save ourselves some suffering.
         ExperienceApp.Initializing += () =>
         {
+            // I'm not 100% sure when the primary input device becomes available. This is to save ourselves some
+            // suffering.
             primaryInput = VRDevice.Device.PrimaryInputDevice;
             avatarRigidbody = GetComponent<Rigidbody>();
+            collider = GetComponent<Collider>();
         };
     }
 
@@ -26,15 +32,25 @@ public class PlayerControl : MonoBehaviour
     {
         // Liminal's SDK seems to recognise my WMR headset as a GearVR device..? As such the controllers aren't
         // functional. This is something to let us spoof controller input only if we want to - it has no runtime cost.
-        #if !DEBUG_VR
+        #if !DEBUG_VR_XDDD
         var movementVector = primaryInput.GetAxis2D(VRAxis.OneRaw).normalized * movementSpeed;
         #else
         var movementVector = new Vector2(
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")).normalized * movementSpeed;
+        
         #endif
 
         var forward = Quaternion.Euler(0f, avatarEye.rotation.eulerAngles.y, 0f);
         avatarRigidbody.velocity = forward * new Vector3(movementVector.x, 0f, movementVector.y);
+    }
+
+    // I hate doing this but without proper layers I'm not sure if there's much other option. Love you Liminal.
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Hands"))
+        {
+            Physics.IgnoreCollision(other.collider, collider);
+        }
     }
 }
