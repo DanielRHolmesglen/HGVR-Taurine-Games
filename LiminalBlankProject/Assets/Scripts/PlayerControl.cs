7a280bd1474498c008025d80e3557a8614573f00
Gareth <gmorgan27@student.holmesglen.edu.au>
2021-05-11 14:11:28 +1000

    Try collisions again

--- LiminalBlankProject/Assets/Scripts/PlayerControl.cs
+++ LiminalBlankProject/Assets/Scripts/PlayerControl.cs
@@ -20,9 +20,6 @@ public class PlayerControl : MonoBehaviour
     {
         ExperienceApp.Initializing += () =>
         {
-            // I'm not 100% sure when the primary input device becomes available. This is to save ourselves some
-            // suffering.
-            primaryInput = VRDevice.Device.PrimaryInputDevice;
             avatarRigidbody = GetComponent<Rigidbody>();
             collider = GetComponent<Collider>();
         };
@@ -33,7 +30,8 @@ public class PlayerControl : MonoBehaviour
         // Liminal's SDK seems to recognise my WMR headset as a GearVR device..? As such the controllers aren't
         // functional. This is something to let us spoof controller input only if we want to - it has no runtime cost.
         #if !DEBUG_VR_XDDD
-        var movementVector = primaryInput.GetAxis2D(VRAxis.OneRaw).normalized * movementSpeed;
+        // I wanted to cache this call but it seems to be updated.
+        var movementVector = VRDevice.Device.PrimaryInputDevice.GetAxis2D(VRAxis.OneRaw).normalized * movementSpeed;
         #else
         var movementVector = new Vector2(
             Input.GetAxisRaw("Horizontal"),
@@ -43,14 +41,5 @@ public class PlayerControl : MonoBehaviour
 
         var forward = Quaternion.Euler(0f, avatarEye.rotation.eulerAngles.y, 0f);
         avatarRigidbody.velocity = forward * new Vector3(movementVector.x, 0f, movementVector.y);
-    }
-
-    // I hate doing this but without proper layers I'm not sure if there's much other option. Love you Liminal.
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.CompareTag("Hands"))
-        {
-            Physics.IgnoreCollision(other.collider, collider);
-        }
     }
 }
