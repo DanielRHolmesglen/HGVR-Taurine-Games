91ddbe7cbf5ab94fa2ab47ca6070e6af1dd87751
WardenTheCorrupt <trevorgordonstott@gmail.com>
2021-05-18 16:16:06 +1000

    Updated room

---
+++ LiminalBlankProject/Assets/PrefabFiles/TurnOnGravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnGravity : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Untagged")
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+            Debug.Log("Whats touching this fucking thing");
+        }
+    }
+
+
+
+
+
+}
