d9f7389eafb0b6c2abf795e8aa3412d69179671b
Gareth <gmorgan27@student.holmesglen.edu.au>
2021-05-18 12:57:41 +1000

    Update player movement system

--- LiminalBlankProject/Assets/Scripts/PlayerControl.cs
+++ LiminalBlankProject/Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,4 @@
-﻿#define DEBUG_VR_XDDDA
-
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using Liminal.SDK.Core;
@@ -8,38 +6,38 @@ using UnityEngine;
 using Liminal.SDK.VR;
 using Liminal.SDK.VR.Input;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
     [SerializeField] private Transform avatarEye;
     private Rigidbody avatarRigidbody;
     private IVRInputDevice primaryInput;
-    private Collider collider;
-    
+    private CharacterController controller;
+
     private void Awake()
     {
-        ExperienceApp.Initializing += () =>
-        {
-            avatarRigidbody = GetComponent<Rigidbody>();
-            collider = GetComponent<Collider>();
-        };
+        ExperienceApp.Initializing += () => { controller = GetComponent<CharacterController>(); };
     }
 
     private void Update()
     {
-        // Liminal's SDK seems to recognise my WMR headset as a GearVR device..? As such the controllers aren't
-        // functional. This is something to let us spoof controller input only if we want to - it has no runtime cost.
-        #if !DEBUG_VR_XDDD
         // I wanted to cache this call but it seems to be updated.
         var movementVector = VRDevice.Device.PrimaryInputDevice.GetAxis2D(VRAxis.OneRaw).normalized * movementSpeed;
-        #else
-        var movementVector = new Vector2(
-            Input.GetAxisRaw("Horizontal"),
-            Input.GetAxisRaw("Vertical")).normalized * movementSpeed;
-        
-        #endif
-
         var forward = Quaternion.Euler(0f, avatarEye.rotation.eulerAngles.y, 0f);
-        avatarRigidbody.velocity = forward * new Vector3(movementVector.x, 0f, movementVector.y);
+        controller.Move(forward * new Vector3(movementVector.x, 0f, movementVector.y) * Time.deltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        var attachedRigidbody = hit.collider.attachedRigidbody;
+        if (attachedRigidbody is null || attachedRigidbody.isKinematic || hit.moveDirection.y < -0.3f ||
+            hit.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        var pushDirection = new Vector3(hit.moveDirection.x, 0f, hit.moveDirection.z);
+        attachedRigidbody.velocity = pushDirection * 3.5f;
     }
 }
