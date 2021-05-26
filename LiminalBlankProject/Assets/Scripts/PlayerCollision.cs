93b7a8b9e642fb6433e2cce8d0fee5f20d640bec
Gareth <gmorgan27@student.holmesglen.edu.au>
2021-05-26 13:53:10 +1000

    Redo movement
    Redo movement, pt. 2
    
    Remove trash

---
+++ LiminalBlankProject/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        var attachedRigidbody = hit.collider.attachedRigidbody;
+        if (attachedRigidbody == null || attachedRigidbody.isKinematic || hit.moveDirection.y < -0.3)
+        {
+            return;
+        }
+        
+        var direction = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        attachedRigidbody.velocity = direction * 3.5f;
+    }
+}
