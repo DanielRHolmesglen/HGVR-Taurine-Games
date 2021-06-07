5aea92f1df512c78028ece9db43e514c6d91c35f
Kaylynn <mkaylynn7@gmail.com>
2021-06-08 09:12:12 +1000

    Add new scripts and clean up a bit

---
+++ LiminalBlankProject/Assets/Scripts/Grabbable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Grabbable : MonoBehaviour
+{
+    public Rigidbody attachedRigidbody;
+
+    private void Start()
+    {
+        attachedRigidbody = GetComponent<Rigidbody>();
+    }
+}
---
+++ LiminalBlankProject/Assets/Scripts/GrabbingHand.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
+using UnityEngine;
+
+public class GrabbingHand : MonoBehaviour
+{
+    public float pickupDistance = 0.3f;
+    private Rigidbody holdingTarget;
+
+    private void FixedUpdate()
+    {
+        var primaryInput = VRDevice.Device.PrimaryInputDevice;
+        var handClosed = primaryInput.GetButton(VRButton.One) || Input.GetMouseButton(0);
+
+        if (!handClosed)
+        {
+            holdingTarget = Physics
+                .OverlapSphere(transform.position, pickupDistance)
+                .Select(collider => collider.GetComponent<Grabbable>()?.attachedRigidbody)
+                .First(collider => collider != null);
+        }
+        
+        if (!holdingTarget) return;
+        
+        var transformRotation = transform.rotation * Quaternion.Inverse(holdingTarget.transform.rotation);
+        // Adjust the velocity of our target to move it towards our hand
+        holdingTarget.velocity = (transform.position - holdingTarget.transform.position) / Time.fixedDeltaTime;
+        holdingTarget.maxAngularVelocity = 20f;
+
+        var newRotation = new Vector3(
+            Mathf.DeltaAngle(0, transformRotation.eulerAngles.x), 
+            Mathf.DeltaAngle(0, transformRotation.eulerAngles.y), 
+            Mathf.DeltaAngle(0, transformRotation.eulerAngles.z));
+        
+        newRotation *= Mathf.Deg2Rad;
+
+        // Adjust the angular velocity of the target to rotate it towards our hand
+        holdingTarget.angularVelocity = newRotation / Time.fixedDeltaTime;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, pickupDistance);
+    }
+}
